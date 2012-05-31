2c80ae43de25caa2486f70f2e570b84c99ff5c5e
Stella <stellamariam@gmail.com>
2012-05-31 16:16:55 -0500

    texting new way to send password by email

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -2,9 +2,9 @@ class MainMailer < ActionMailer::Base
   
   default :from => "\"iWarn\"<no-reply@iWarn.com>" 
 
-  def added_as_a_user (to,name,password)
-    @name => name
-    @password => password
+  def added_as_a_user (to, name, password)
+    @name = name
+    @password = password
     mail(:to => to, :subject => "iWarn - Congrats!" ) 
 
   end
